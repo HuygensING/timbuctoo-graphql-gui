8992626a7272f1f7a3a0080b82676d5505606a59
Martijn Maas <martijn.maas@huygens.knaw.nl>
2017-05-09 16:35:29 +0200

    Clean up code

--- src/app/components/api.tsx
+++ src/app/components/api.tsx
@@ -5,7 +5,7 @@ export interface DataRenderer {
   fields(): string[]; // returns an empty array if the DataRenderer has no fields
   getData(): any;
   renderField(fieldName: string | number): JSX.Element; // returns an empty span for unknown fields
-  subRenderer(fieldName: string): DataRenderer; // returns a null DataRenderer for unknown field
+  subRenderer(fieldName: string | number): DataRenderer; // returns a null DataRenderer for unknown field
 }
 
 export interface TimComponent {
@@ -16,7 +16,7 @@ export function Entity(props: {datarenderer: DataRenderer}): JSX.Element {
   const properties: JSX.Element[] = [];
 
   for (const field of props.datarenderer.fields()) {
-      properties.push(props.datarenderer.renderField(field));
+    properties.push(props.datarenderer.renderField(field));
   }
 
   return <div>{properties}</div>;
